4cb9d159cd8962c7580ce61ed1f351edae82a3dd
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-07-22 10:17:51 +0300

    Initial Commit

---
+++ BreakpointToggle/BreakpointToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakpointToggleApp.swift
+//  BreakpointToggle
+//
+//  Created by ramil on 22.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct BreakpointToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
